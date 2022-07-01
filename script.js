344c8e3665ed4263b7621c94ccdefe7eadcfd61c
Aigul <sharipova.aig@gmail.com>
2022-07-01 15:47:46 -0400

    function const1

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 const search = document.querySelector('.search');
 const photo = document.querySelectorAll('.photo');
+search.addEventListener ('keyup', function(event) {
+  const nameBook = event.target.value.toLowerCase();
+  
+})
