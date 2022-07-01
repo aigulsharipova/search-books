0b6c043a86c7ebc302b8bb05e2eb26741cb7520d
Aigul <sharipova.aig@gmail.com>
2022-07-01 16:03:37 -0400

    functions

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const search = document.querySelector('.search');
 const photo = document.querySelectorAll('.photo');
-search.addEventListener ('keyup', function(event) {
+
+search.addEventListener('keyup', function(event) {
   const nameBook = event.target.value.toLowerCase();
   const nameAuthor = event.target.value.toLowerCase();
   const booksType = event.target.value.toLowerCase();
+
+  photo.forEach(item => {item.querySelector('p').textContent.toLowerCase.includes(nameBook) || item.querySelector('.author').textContent.toLowerCase().includes(nameAuthor) || item.querySelector('.type').textContent.toLowerCase().includes(booksType) ? (item.style.display = 'block') : (item.style.display = 'none');
+})
 })
